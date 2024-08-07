0c78e34b24422af71be625059a6f97383d9e9191
cyxiwai <A15618827017@gmail.com>
2024-08-07 20:09:29 +0800

    多线程并发

---
+++ src/main/java/com/xiwai/threadsafe/test1.java
@@ -0,0 +1,10 @@
+package com.xiwai.threadsafe;
+
+public class test1 {
+    public static void main(String[] args) {
+        ThreadUnsafeExample threadUnsafeExample=new ThreadUnsafeExample();
+    }
+}
+class ThreadUnsafeExample{
+    
+}
